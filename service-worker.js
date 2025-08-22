4e78d3a35d88ca51aa8054af8ecd5ce052161ae3
Mohd Suhail <58903056+mohammedsuhail464@users.noreply.github.com>
2025-08-22 23:41:57 +0100

    Add files via upload
    Initial portfolio release

---
+++ service-worker.js
@@ -0,0 +1,13 @@
+const CACHE_NAME = 'portfolio-v1';
+const ASSETS = [
+  './',
+  'index.html',
+  'manifest.json',
+  'Mohd_Suhail_CV.pdf'
+];
+self.addEventListener('install', e =>
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)))
+);
+self.addEventListener('fetch', e =>
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)))
+);
